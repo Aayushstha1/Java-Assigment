e6c7d95c3d4dd718a1c10f49bd01c689a92cd484
Aayush stha <sudipk339@gmail.com>
2024-03-13 23:16:47 +0545

    Factorial

--- factorial.java
+++ factorial.java
@@ -1,3 +1,22 @@
+
+import java.util.Scanner;
+
 public class factorial {
-    
+    //WAP to find the factorial of a user entered number
+
+    public static void main(String []args)
+    {
+        Scanner number = new Scanner(System.in);
+        System.out.println("Enter the number to get the factorial of: ");
+        int num = number.nextInt();
+        int copyNum=num;
+        int fact=1;
+        while(num>0)
+        {
+            fact=fact*num;
+            num--;
+        }
+        System.out.print("Factorial of " +copyNum+ " is " +fact);
+    }
 }
+
